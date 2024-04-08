291cbdb15acdc0a94300598c46b1d8bcb49615bc
hannarubensson <hanna.rubensson@gmail.com>
2024-04-08 18:03:37 +0200

    first draft for today

--- script.js
+++ script.js
@@ -22,13 +22,8 @@ class PokemonCharacter {
             console.log("Error fetching pokemon:", error); 
             throw error; 
         }
-
     }
-
-
 }
-
-
 
 class CreateCharacter {
 
@@ -43,12 +38,12 @@ class CreateCharacter {
         const defaultPic = sprites.front_default;
         
         
-        
+        // Iterera igenom arrayen
         types.forEach(type => {
-            typesList += `<h3>Type: ${type.type.name}</h3>`;
+            typesList += `<h3><u>Type:</u> ${type.type.name}</h3>`;
         });
 
-        // Forloop för att gå igenom bilderna (så inte alla bilder visas ut)
+        // Iterera igenom bilderna i objektet
         for (const key in sprites) {
             if (sprites.hasOwnProperty(key)) {
                 if (key === 'front_default' && defaultPic) {
@@ -58,17 +53,17 @@ class CreateCharacter {
         }
 
         stats.forEach(stat => {
-            statsList += `<h3>${stat.stat.name}: ${stat.base_stat}</h3>`;
+            statsList += `<h3><u>${stat.stat.name}:</u> ${stat.base_stat}</h3>`;
         });
 
 
         characterDiv.innerHTML = `
             <div id="character-info-card">
                 ${pic}
-                <h3>Name: ${name}</h3>
+                <h3><u>Name:</u> ${name}</h3>
                 ${typesList}
-                <h3>Weight: ${weight}</h3>
-                <h3>Height: ${height}</h3>
+                <h3><u>Weight:</u> ${weight}</h3>
+                <h3><u>Height:</u> ${height}</h3>
                 ${statsList}
             </div>
         `;
@@ -79,15 +74,64 @@ class CreateCharacter {
         
     }
 
+    static createButton() {
+
+        let compareBtn = document.createElement("button");
+        const wrapper = document.getElementById("fighter-wrapper"); 
+        compareBtn.setAttribute("id", "compare-pokemon-button"); 
+        compareBtn.innerHTML = `Compare Pokemons<br>
+        <i class="fa fa-arrows-h" style="font-size:36px"></i>`; 
+        wrapper.append(compareBtn); 
+        
+    }
+
 
 
 }
+class Interaction {
 
-class CompareCharacters {
+    constructor(poke1, poke2) {
+        this.poke1 = poke1; 
+        this.poke2 = poke2; 
+    }
+
+    getStats(poke1, poke2) {
+
+        const stats = {};
+    
+        poke1.stats.forEach(stat => {
+            stats[stat.stat.name] = stat.base_stat;
+        });
+
+        poke2.stats.forEach(stat => {
+            stats[stat.stat.name] = stat.base_stat;
+        });
+    
+            return stats;
+        
+    }
+
+    compareCharacters(poke1, poke2) {
+
+        poke1 = this.poke1;
+        poke2 = this.poke2;
+
+        const poke1Stats = this.getStats(poke1);
+        const poke2Stats = this.getStats(poke2);
+
+        console.log(`${poke1.name} stats:`, poke1Stats);
+        console.log(`${poke2.name} stats:`, poke2Stats);
+
+        // Hitpoints, attack, special attack, defense, special defense, speed
+
+    }
+
+    fight(poke1, poke2) {
 
 
+    }
+    
 }
-
 
 let selectFighterBtn = document.getElementById("select-fighter"); 
 
@@ -107,6 +151,7 @@ selectFighterBtn.addEventListener("click", async () => {
         ]);
 
         CreateCharacter.createCharacterDiv(pokemon1);
+        CreateCharacter.createButton(); 
         CreateCharacter.createCharacterDiv(pokemon2);
 
         console.log(pokemon1);
@@ -115,6 +160,23 @@ selectFighterBtn.addEventListener("click", async () => {
     } catch (error) {
         console.error('Error:', error);
     }
+
+
+    let compareBtn = document.getElementById("compare-pokemon-button"); 
+
+    // BUTTON FOR COMPARING POKEMONS
+    compareBtn.addEventListener("click", () => {
+
+        console.log("klickat!");
     
+        let poke1 = document.getElementById("fighter-one").value;
+        let poke2 = document.getElementById("fighter-two").value;
+
+        const interaction = new Interaction(poke1, poke2);
+        interaction.compareCharacters(poke1, poke2); 
+
+
 });
 
+
+});
